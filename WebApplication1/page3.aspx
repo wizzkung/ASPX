1a81d87df0e8e4512e8a1bd55519c3307bbeb795
Wizzkung <sandtorus@gmail.com>
2024-11-26 13:24:18 +0500

    aspx calc

---
+++ WebApplication1/page3.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="page3.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <asp:TextBox ID="TextBox1" runat="server" Height="32px" Width="79px"></asp:TextBox><br />
+            <asp:Button ID="Button13" runat="server" Text="C" OnClick="Clear_Click"/>
+        <asp:Button ID="divide" runat="server" Text="/" OnClick="Operation_Click"/>
+        <br />
+            <asp:Button ID="one" runat="server" Text="1" OnClick="Num_Click"/>
+            <asp:Button ID="two" runat="server" Text="2" OnClick="Num_Click"/>
+            <asp:Button ID="three" runat="server" Text="3" OnClick="Num_Click"/>
+        <asp:Button ID="plus" runat="server" Text="+" OnClick="Operation_Click" />
+    
+        <br />
+   
+        <asp:Button ID="four" runat="server" Text="4" OnClick="Num_Click"/>
+        <asp:Button ID="five" runat="server" Text="5" OnClick="Num_Click"/>
+        <asp:Button ID="six" runat="server" Text="6" OnClick="Num_Click"/>
+        <asp:Button ID="minus" runat="server" Text="-" OnClick="Operation_Click"/>
+        <br />
+            <asp:Button ID="seven" runat="server" Text="7" OnClick="Num_Click"/>
+            <asp:Button ID="eight" runat="server" Text="8" OnClick="Num_Click"/>
+            <asp:Button ID="nine" runat="server" Text="9" OnClick="Num_Click"/>
+          <asp:Button ID="multiply" runat="server" Text="*" OnClick="Operation_Click"/><br />
+        
+        <asp:Button ID="zero" runat="server" Text="0" OnClick="Num_Click"/>
+        
+        <asp:Button ID="enter" runat="server" Text="Enter" OnClick="Enter_Click"/>
+        
+    </form>
+</body>
+</html>
---
+++ WebApplication1/page3.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static double _firstNumber = 0;
+        private static double _secondNumber = 0;
+        private static string _operation = string.Empty;
+        private static bool _isResultDisplayed = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Num_Click(object sender, EventArgs e)
+        {
+                Button button = (Button)sender;
+            if (_isResultDisplayed)
+            {
+                TextBox1.Text = string.Empty;
+                _isResultDisplayed = false;
+            }
+                TextBox1.Text += button.Text;
+        }
+
+        protected void Operation_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (double.TryParse(TextBox1.Text, out double result))
+            {
+                _firstNumber = result;
+                _operation = button.Text;
+                TextBox1.Text += " " + _operation + " ";
+            }
+            else {
+                TextBox1.Text = "Ошибка";
+            }
+        }
+
+        protected void Enter_Click(object sender, EventArgs e)
+        {
+            string[] parts = TextBox1.Text.Split(' ');
+
+            if (parts.Length == 3 &&
+                double.TryParse(parts[2], out _secondNumber)) // Получаем второе число
+            {
+                double result = 0;
+                switch (_operation)
+                {
+                    case "+":
+                        result = _firstNumber + _secondNumber;
+                        break;
+                    case "-":
+                        result = _firstNumber - _secondNumber;
+                        break;
+                    case "*":
+                        result = _firstNumber * _secondNumber;
+                        break;
+                    case "/":
+                        result = _secondNumber != 0 ? _firstNumber / _secondNumber : double.NaN;
+                        break;
+                    default:
+                        TextBox1.Text = "Ошибка!";
+                        return;
+                }
+
+                TextBox1.Text = result.ToString(); // Отображаем результат
+                _firstNumber = 0;
+                _secondNumber = 0;
+                _operation = string.Empty;
+                _isResultDisplayed = true;
+            }
+            else
+            {
+                TextBox1.Text = "Ошибка!";
+            }
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            _firstNumber = 0;
+            _secondNumber = 0;
+            _operation = string.Empty;
+            _isResultDisplayed = false;
+            TextBox1.Text = string.Empty;
+
+        }
+    }
+}
